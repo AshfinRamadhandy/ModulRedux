438990806927ab098ea365974b447eb30301ef22
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2020-03-19 17:01:09 +0700

    FilterReducer

--- redux/src/reducers/FilterReducer.js
+++ redux/src/reducers/FilterReducer.js
@@ -1,9 +1,12 @@
-import { SHOW_ALL, SET_VISIBILITY_FILTER } from "../actions/actionsTypes";
+import { SHOW_ALL, SET_VISIBILITY_FILTER } from "../Action/actionTypes";
 
-const visibilityFilter = (state = SHOW_ALL, action) => { switch (action.type) {
+const visibilityFilter = (state = SHOW_ALL, action) => {
+    switch (action.type) {
         case SET_VISIBILITY_FILTER:
             return action.filter;
         default:
-            return state; } };
+            return state;
+    }
+};
 
 export default visibilityFilter;
